b58eca6004646971104cfcaae03a5ca6b3b7163c
DerpDays <34582078+DerpDays@users.noreply.github.com>
2026-01-21 03:09:21 +0000

    initial before testing

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+#![feature(impl_trait_in_assoc_type)]
+
+use embassy_executor::Spawner;
+use embassy_rp::gpio::{Level, Output};
+use embassy_time::{Duration, Timer};
+use panic_halt as _;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let peripherals = embassy_rp::init(Default::default());
+    let mut led = Output::new(peripherals.PIN_25, Level::Low);
+
+    loop {
+        led.set_high();
+        Timer::after(Duration::from_millis(50)).await;
+        led.set_low();
+        Timer::after(Duration::from_millis(950)).await;
+    }
+}
