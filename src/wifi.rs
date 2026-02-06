f0ffeff0a4c4501090b968374711ddc512d22050
Alexandre <git@alexdias.dev>
2026-02-06 03:08:22 +0000

    Use ManuallyDrop for wifi pio fields

--- src/wifi.rs
+++ src/wifi.rs
@@ -1,3 +1,5 @@
+use core::mem::ManuallyDrop;
+
 use cyw43::{Control, NetDriver, Runner};
 use cyw43_pio::{DEFAULT_CLOCK_DIVIDER, PioSpi};
 use embassy_rp::{
@@ -18,21 +20,10 @@ pub async fn cyw43_task(
 const FIRMWARE: &[u8] = include_bytes!("../cyw43-firmware/43439A0.bin");
 pub const CLM: &[u8] = include_bytes!("../cyw43-firmware/43439A0_clm.bin");
 
-#[allow(unused)]
 pub struct Wifi {
     pub driver: NetDriver<'static>,
     pub control: Control<'static>,
     pub runner: Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>,
-
-    // rest of the Pio fields.
-    common: embassy_rp::pio::Common<'static, PIO0>,
-    irq_flags: embassy_rp::pio::IrqFlags<'static, PIO0>,
-    irq1: embassy_rp::pio::Irq<'static, PIO0, 1>,
-    irq2: embassy_rp::pio::Irq<'static, PIO0, 2>,
-    irq3: embassy_rp::pio::Irq<'static, PIO0, 3>,
-    sm1: embassy_rp::pio::StateMachine<'static, PIO0, 1>,
-    sm2: embassy_rp::pio::StateMachine<'static, PIO0, 2>,
-    sm3: embassy_rp::pio::StateMachine<'static, PIO0, 3>,
 }
 
 impl Wifi {
@@ -46,50 +37,30 @@ impl Wifi {
     ) -> Self {
         let pwr = Output::new(pin_23, Level::Low);
         let cs = Output::new(pin_25, Level::High);
-        let pio = Pio::new(pio_0, crate::Irqs);
 
-        let Pio {
-            mut common,
-            irq_flags,
-            irq0,
-            irq1,
-            irq2,
-            irq3,
-            sm0,
-            sm1,
-            sm2,
-            sm3,
-            ..
-        } = pio;
+        let mut pio = ManuallyDrop::new(Pio::new(pio_0, crate::Irqs));
+        // SAFETY: we want to leak this, and since everything is 'static, it is safe to do so.
+        let mut pio = unsafe { ManuallyDrop::take(&mut pio) };
 
         let spi = PioSpi::new(
-            &mut common,
-            sm0,
+            &mut pio.common,
+            pio.sm0,
             DEFAULT_CLOCK_DIVIDER,
-            irq0,
+            pio.irq0,
             cs,
             pin_24,
             pin_29,
             dma_ch0,
         );
 
-        static STATE: StaticCell<cyw43::State> = StaticCell::new();
-        let state = STATE.init(cyw43::State::new());
+        static WIFI_STATE: StaticCell<cyw43::State> = StaticCell::new();
+        let state = WIFI_STATE.init(cyw43::State::new());
 
         let (driver, control, runner) = cyw43::new(state, pwr, spi, FIRMWARE).await;
         Self {
             driver,
             control,
             runner,
-
-            common,
-            irq_flags,
-            irq1,
-            irq2,
-            irq3,
-            sm1,
-            sm2,
-            sm3,
         }
     }
 }
